56c749f1667571267177d70f2680fe8d1222e8ca
Silpux <101855190+Silpux@users.noreply.github.com>
2025-07-02 01:49:23 +0300

    add tooltip helper classes

---
+++ GrowCastleAutobot/gca_clicker/Classes/TooltipHelper.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace gca_clicker.Classes
+{
+    public static class TooltipHelper
+    {
+        public static readonly DependencyProperty EnabledTooltipProperty =
+            DependencyProperty.RegisterAttached(
+                "EnabledTooltip",
+                typeof(string),
+                typeof(TooltipHelper),
+                new FrameworkPropertyMetadata(null));
+
+        public static void SetEnabledTooltip(UIElement element, string value)
+        {
+            element.SetValue(EnabledTooltipProperty, value);
+        }
+
+        public static string GetEnabledTooltip(UIElement element)
+        {
+            return (string)element.GetValue(EnabledTooltipProperty);
+        }
+
+        public static readonly DependencyProperty DisabledTooltipProperty =
+            DependencyProperty.RegisterAttached(
+                "DisabledTooltip",
+                typeof(string),
+                typeof(TooltipHelper),
+                new FrameworkPropertyMetadata(null));
+
+        public static void SetDisabledTooltip(UIElement element, string value)
+        {
+            element.SetValue(DisabledTooltipProperty, value);
+        }
+
+        public static string GetDisabledTooltip(UIElement element)
+        {
+            return (string)element.GetValue(DisabledTooltipProperty);
+        }
+    }
+}
